fa3f0a10ddfd0edf3369e899d9b6d08b79c90c98
Ahmed-Saad <Ahmed-Saad@AHMED>
2025-03-08 21:00:36 +0200

    Create index(Action-View)

--- Company.G02.DAL/Models/Department.cs
+++ Company.G02.DAL/Models/Department.cs
@@ -9,8 +9,8 @@ namespace Company.G02.DAL.Models
    public class Department
     {
         public int Id { get; set; }
-        public string? Code { get; set; }
-        public string? Name { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
         public DateTime CreateAt { get; set; }
     }
 }
