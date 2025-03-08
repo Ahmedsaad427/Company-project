e6d95117644366243d1c852d6a9f89f3eed4eaa8
Ahmed-Saad <Ahmed-Saad@AHMED>
2025-03-08 04:48:11 +0200

    Implementaion of  BLL

---
+++ Company.G02.DAL/Data/Contexts/CompanyDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Company.G02.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Reflection;
+
+namespace Company.G02.DAL.Data.Contexts
+{
+    public class CompanyDbContext : DbContext
+    {
+        public CompanyDbContext():base()
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=Ahmed;Database=CompanyG02;Trusted_Connection=True;TrustServerCertificate=True"
+            );
+        }
+        public DbSet<Department>Departments { get; set; }
+    }
+}
