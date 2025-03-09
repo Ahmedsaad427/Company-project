cc9a0c9d7a77f2f0dcd5bc2c5d6076a6e14d1932
Ahmed-Saad <Ahmed-Saad@AHMED>
2025-03-09 20:00:31 +0200

    Update Part

---
+++ Company.G02.PL/Controllers/DeleteController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Company.G02.BLL.Interfaces;
+
+namespace Company.G02.PL.Controllers
+{
+    public class DeleteController : Controller
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public DeleteController(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+
+        public IActionResult Index(int id)
+        {
+            var department = _departmentRepository.Get(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return View(department);
+        }
+
+        [HttpPost]
+        public IActionResult ConfirmDelete(int id)
+        {
+            var department = _departmentRepository.Get(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            _departmentRepository.Delete(department);
+
+            // 🚀 Redirect to the Department list after successful delete
+            return RedirectToAction("Index", "DepartmentControllers");
+        }
+    }
+}
